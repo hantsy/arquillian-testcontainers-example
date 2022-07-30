629da22072e327f3b6278186d0ae5b2a6e24758b
hantsy <hantsy@gmail.com>
2022-07-30 15:55:20 +0800

    chore: init commit

---
+++ src/test/java/com/example/it/GreetingResourceDeployment.java
@@ -0,0 +1,31 @@
+package com.example.it;
+
+import com.example.GreetingMessage;
+import com.example.GreetingResource;
+import com.example.GreetingService;
+import com.example.JaxrsActivator;
+import org.jboss.arquillian.container.test.api.DeploymentConfiguration;
+import org.jboss.arquillian.container.test.api.DeploymentConfiguration.DeploymentContentBuilder;
+import org.jboss.arquillian.container.test.spi.client.deployment.AutomaticDeployment;
+import org.jboss.arquillian.test.spi.TestClass;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+public class GreetingResourceDeployment implements AutomaticDeployment {
+    @Override
+    public DeploymentConfiguration generateDeploymentScenario(TestClass testClass) {
+        var war = ShrinkWrap.create(WebArchive.class)
+                .addClass(GreetingMessage.class)
+                .addClass(GreetingService.class)
+                .addClass(GreetingResource.class)
+                .addClass(JaxrsActivator.class)
+                // Enable CDI (Optional since Java EE 7.0)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+
+        return new DeploymentContentBuilder(war)
+                .withDeployment()
+                .build()
+                .get();
+    }
+}
---
+++ src/test/java/com/example/it/GreetingResourceTest.java
@@ -0,0 +1,85 @@
+package com.example.it;
+
+import com.example.GreetingMessage;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.arquillian.container.test.api.BeforeDeployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(ArquillianExtension.class)
+@Testcontainers
+public class GreetingResourceTest {
+    private final static Logger LOGGER = Logger.getLogger(GreetingResourceTest.class.getName());
+
+    @Container
+    static GenericContainer wildfly = new GenericContainer<>(
+            DockerImageName
+                    .parse("quay.io/wildfly/wildfly")
+                    .asCompatibleSubstituteFor("jboss/wildfly")
+    )
+            .withExposedPorts(8080, 9990)
+            .withCreateContainerCmdModifier(cmd -> cmd.withCmd("/opt/jboss/wildfly/bin/add-user.sh admin Admin@123 --silent").exec())
+            .withCommand("/opt/jboss/wildfly/bin/standalone.sh", "-b", "0.0.0.0", "-bmanagement", "0.0.0.0");
+
+    @BeforeDeployment
+    public static Archive<?> beforeDeployment(Archive<?> archive) {
+        Wait.forListeningPort().waitUntilReady(wildfly);
+        LOGGER.log(Level.INFO, "deployment files: {}", archive.toString(true));
+        return archive;
+    }
+
+    @ArquillianResource
+    private URL base;
+
+    private Client client;
+
+    @BeforeEach
+    public void setup() {
+        LOGGER.info("call BeforeEach");
+        this.client = ClientBuilder.newClient();
+        //removed the Jackson json provider registry, due to OpenLiberty 21.0.0.1 switched to use Resteasy.
+    }
+
+    @AfterEach
+    public void teardown() {
+        LOGGER.info("call AfterEach");
+        if (this.client != null) {
+            this.client.close();
+        }
+    }
+
+    @Test
+    @DisplayName("Given a name:`JakartaEE` should return `Say Hello to JakartaEE`")
+    public void should_create_greeting() throws MalformedURLException {
+        LOGGER.log(Level.INFO, " client: {0}, baseURL: {1}", new Object[]{client, base});
+        final var greetingTarget = this.client.target(new URL(this.base, "api/greeting/JakartaEE").toExternalForm());
+        try (final Response greetingGetResponse = greetingTarget.request()
+                .accept(MediaType.APPLICATION_JSON)
+                .get()) {
+            assertThat(greetingGetResponse.getStatus()).isEqualTo(200);
+            assertThat(greetingGetResponse.readEntity(GreetingMessage.class).getMessage()).startsWith("Say Hello to JakartaEE");
+        }
+    }
+}
